564a66d4a6c23da4cdeb5a942eed236070195c7f
ajguerra <arthurjguerra@gmail.com>
2017-06-11 15:19:26 -0300

    Implemented tests for delete and delete all campaigns

---
+++ src/test/java/CampaignServiceDeleteTest.java
@@ -0,0 +1,50 @@
+import core.Application;
+import core.Campaign;
+import core.DateUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import service.CampaignService;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test Campaign controller.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class CampaignServiceDeleteTest {
+
+    private static final String TEST_CAMPAIGN_NAME = "testCampaign";
+
+    @Autowired
+    private CampaignService campaignService;
+
+    private Campaign campaign1, campaign2;
+
+    @Before
+    public void setUp() {
+        campaign1 = campaignService.create(TEST_CAMPAIGN_NAME, 1, DateUtils.today(), DateUtils.tomorrow());
+        campaign2 = campaignService.create(TEST_CAMPAIGN_NAME, 2, DateUtils.today(), DateUtils.today());
+    }
+
+    @Test
+    public void deleteAllCampaigns() {
+        campaignService.deleteAll();
+        assertTrue(campaignService.findAllValidCampaigns().isEmpty());
+    }
+
+    @Test
+    public void deleteCampaign() {
+        campaignService.delete(campaign1.getId());
+        assertEquals(1, campaignService.findAllValidCampaigns().size());
+        assertEquals(campaign2.getId(), campaignService.findAllValidCampaigns().get(0).getId());
+    }
+
+}
